949e514061c48df595ccaf86f1c7bd86b6bc2769
ExposedCat <artem13.prokop@gmail.com>
2021-06-27 13:41:45 +0300

    Add db config

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+const pgPromiseConfiguration = {
+    host: 'localhost',
+    port: 5432,
+    database: 'shop-db',
+    user: 'shop-admin',
+    password: 'uck202urP',
+    max: 30
+}
+
+export {
+    pgPromiseConfiguration
+}
