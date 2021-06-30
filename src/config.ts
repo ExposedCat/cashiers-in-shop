fa6dfe556a7a2f6c762c405cb0fefa48e995fade
ExposedCat <artem13.prokop@gmail.com>
2021-06-30 15:44:44 +0300

    Add ability to use env file with DB auth data

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 const pgPromiseConfiguration = {
-    host: 'localhost',
-    port: 5432,
-    database: 'shop-db',
-    user: 'shop-admin',
-    password: 'uck202urP',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: Number(process.env.POSTGRES_PORT) || 5432,
+    database: process.env.POSTGRES_DB || 'shop-db',
+    user: process.env.POSTGRES_USER || 'shop-admin',
+    password: process.env.POSTGRES_PASSWORD || 'uck202urP',
     max: 30
 }
 
